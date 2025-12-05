2dfcf644dc2cad7b17d061c495c987a9d5253887
Ben Stuart <ben@benstuart.ie>
2025-12-05 18:11:26 +0000

    Add gnome fav apps

--- modules/home/gnome/default.nix
+++ modules/home/gnome/default.nix
@@ -1,10 +1,22 @@
-{ lib, pkgs, username, osConfig, config, ... }: {
+{ config, lib, pkgs, username, osConfig, ... }: {
   
   options = {
     gnome.enable = lib.mkOption {
       type = lib.types.bool;
       default = osConfig.gnome.enable or false;
       description = "Enable GNOME specific settings and packages.";
+    };
+    gnome.favoriteApps = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [
+        "org.gnome.Nautilus.desktop"
+        "firefox.desktop"
+        "com.mitchellh.ghostty.desktop"
+        "1password.desktop"
+        "obsidian.desktop"
+        "zotero.desktop"
+      ];
+      description = "List of favorite applications to show in the GNOME dock.";
     };
   };
 
@@ -18,16 +30,20 @@
       enable = true;
       settings = {
         "org/gnome/shell" = {
-          disable-user-extensions = false;
           disabled-extensions = with pkgs.gnomeExtensions; [];
           enabled-extensions = with pkgs.gnomeExtensions; [
             appindicator.extensionUuid
           ];
+
+          favorite-apps = config.gnome.favoriteApps;
         };
 
         "org/gnome/desktop/interface" = {
           color-scheme = "prefer-dark";
           avatar-directories = ["/home/${username}/"];
+          gtk-theme = "Adwaita";
+          icon-theme = "Adwaita";
+          cursor-theme = "Adwaita";
         };
         "org/gnome/desktop/background".picture-uri-dark = "file:///home/${username}/.config/userdata/wallpaper.jpg";
 
