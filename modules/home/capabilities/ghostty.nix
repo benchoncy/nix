f65c58bcf2d09dcfc7236e3e7a0137be41ad11d6
Ben Stuart <ben@benstuart.ie>
2026-03-20 16:21:01 +0000

    Blinking block cursor

--- modules/home/capabilities/ghostty.nix
+++ modules/home/capabilities/ghostty.nix
@@ -2,6 +2,9 @@
   xdg.configFile."ghostty/config".text = ''
     theme = Catppuccin Macchiato
     font-family = Hack Nerd Font Mono
+    cursor-style = block
+    cursor-style-blink = true
+    cursor-invert-fg-bg = true
   '' + lib.optionalString pkgs.stdenv.isDarwin ''
     font-size = 17.0
   '';
