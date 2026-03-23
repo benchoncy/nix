5e181221037f908925acb6c1f73d14f690701166
Ben Stuart <ben@benstuart.ie>
2026-03-23 13:47:44 +0000

    Update cursor config

--- modules/home/capabilities/ghostty.nix
+++ modules/home/capabilities/ghostty.nix
@@ -5,6 +5,7 @@
     cursor-style = block
     cursor-style-blink = true
     cursor-invert-fg-bg = true
+    shell-integration-features = no-cursor
   '' + lib.optionalString pkgs.stdenv.isDarwin ''
     font-size = 17.0
   '';
