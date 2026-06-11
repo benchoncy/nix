bb14bdaf3d6c6ee458e49de9ac50862ed19669a2
Ben Stuart <ben@benstuart.ie>
2026-06-11 09:45:43 +0100

    Start podman machine at start for darwin

--- modules/home/programs/podman.nix
+++ modules/home/programs/podman.nix
@@ -1,6 +1,24 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
   home.packages = with pkgs; [
     podman
     podman-desktop
   ];
+
+  launchd.agents.podman-machine = lib.mkIf pkgs.stdenv.isDarwin {
+    enable = true;
+    config = {
+      ProgramArguments = [
+        "/bin/sh"
+        "-lc"
+        ''
+          state="$(${pkgs.podman}/bin/podman machine inspect podman-machine-default --format '{{.State}}' 2>/dev/null || true)"
+
+          if [ "$state" != "running" ]; then
+            exec ${pkgs.podman}/bin/podman machine start --no-info podman-machine-default
+          fi
+        ''
+      ];
+      RunAtLoad = true;
+    };
+  };
 }
